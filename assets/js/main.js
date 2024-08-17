bdbe0f7f7b0ce6d9f8a3e8d1887107f03024910d
MinaPhan2815 <minhnhatphan2815@gmail.com>
2024-08-17 10:36:38 +0700

    add ScrollReveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,3 +120,16 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay:400,
+    // reset: true
+})
+
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
+sr.reveal(`.home__name, .home__info,
+            .about__container .section__title-1, .about__info,
+            .contact__data, .contact__social`, {origin: 'left'})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
